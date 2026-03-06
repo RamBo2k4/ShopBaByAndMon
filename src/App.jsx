5520a2e19c5478be82dccfbc33cacc9d53dff7cd
van nguyen <nguyenvannguyen2552004@gmail.com>
2026-03-06 18:00:05 +0700

    Merge: code

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import Header from './components/header'
 import ProductCard from './components/ProductCard'
 import Footer from './pages/Footer'
 import MenuMain from './pages/MenuMain'
-import Footer from './pages/Footer'
-import ProductCard from './components/ProductCard'
-
 function App() {
   return (
     <>
